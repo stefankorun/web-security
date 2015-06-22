cbcb76be214443d602eeedb03237d108aaf5ea19
Stefan <korun_stefan@hotmail.com>
2015-06-22 21:41:15 +0200

    persist data to mongo, save xssText

--- src/app/main/xss/xss.controller.js
+++ src/app/main/xss/xss.controller.js
@@ -6,11 +6,25 @@
     .controller('XssViewController', XssViewController);
 
   /** @ngInject */
-  function XssViewController($scope) {
+  function XssViewController($scope, MongoService, toastr) {
     var codeHolder = $('.unfiltered section');
+    MongoService.readData('xssText').success(function (res) {
+      $scope.xssText = res.data;
+    });
+
 
     $scope.$watch('xssText', function (text) {
       codeHolder.html(text);
     });
+
+    $scope.saveXssText = function (string) {
+      MongoService.saveData('xssText', string)
+        .success(function () {
+          toastr.info('Saved');
+        })
+        .error(function () {
+          toastr.error('Error');
+        });
+    }
   }
 })();
---
+++ src/app/services/mongo.js
@@ -0,0 +1,37 @@
+/* global FB:false, gapi:false */
+(function () {
+  'use strict';
+
+  angular
+    .module('webSecurity')
+    .service('MongoService', MongoService);
+
+  /** @ngInject */
+  function MongoService($http) {
+    var apiKey = '?apiKey=XEc7QHCEd0-ziGzLAgA7bxfHvB2zkgy1';
+    var dbName = 'mws-finki';
+
+
+    this.getDatabases = function () {
+      $http.get('https://api.mongolab.com/api/1/databases' + apiKey).then(function (res) {
+        console.log(res);
+      })
+    };
+    this.getCollections = function () {
+      $http.get('https://api.mongolab.com/api/1/databases/' + dbName + '/collections' + apiKey).then(function (res) {
+        console.log(res);
+      })
+    };
+    this.saveData = function (key, data) {
+      var mongoObj = {
+        _id: key,
+        data: data
+      };
+
+      return $http.post('https://api.mongolab.com/api/1/databases/' + dbName + '/collections/persist' + apiKey, mongoObj);
+    };
+    this.readData = function (key) {
+      return $http.get('https://api.mongolab.com/api/1/databases/' + dbName + '/collections/persist/' + key + apiKey);
+    };
+  }
+})();
